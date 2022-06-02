613971536388bac7e36cb7ea5b55f98f5b290124
Flobul <46993341+Flobul@users.noreply.github.com>
2022-06-02 19:26:04 +0200

    On installation, only remove cron on auto mode

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -22,15 +22,17 @@ function hdsentinel_install() {
 
     message::add('hdsentinel', 'Cette mise à jour peut durer un certain temps : elle va stopper puis supprimer les cron actuels sur les appareils distants, puis renvoyer les scripts, installer les paquets le cron et le lancer.');
     foreach (eqLogic::byType('hdsentinel') as $eqLogic) {
-        $result = array();
-        //$result[] = $eqLogic->stopCron();
-        log::add('hdsentinel', 'info', __('=> Début mise à jour des scripts', __FILE__) . $eqLogic->getName());
-        $result['stop_remove'] = $eqLogic->removeCron();
-        $result['send'] = $eqLogic->sendFile();
-        $result['install'] = $eqLogic->installDependancy();
-        $result['launch'] = $eqLogic->launchCron();
-        $result['status'] = $eqLogic->statusCron();
-        log::add('hdsentinel', 'info', __('=> Fin mise à jour des scripts', __FILE__) . $eqLogic->getName() . ' - resultat : ' . json_encode($result));
+        if (!$eqLogic->getConfiguration('manually', false)) {
+            $result = array();
+            //$result[] = $eqLogic->stopCron();
+            log::add('hdsentinel', 'info', __('=> Début mise à jour des scripts', __FILE__) . $eqLogic->getName());
+            $result['stop_remove'] = $eqLogic->removeCron();
+            $result['send'] = $eqLogic->sendFile();
+            $result['install'] = $eqLogic->installDependancy();
+            $result['launch'] = $eqLogic->launchCron();
+            $result['status'] = $eqLogic->statusCron();
+            log::add('hdsentinel', 'info', __('=> Fin mise à jour des scripts', __FILE__) . $eqLogic->getName() . ' - resultat : ' . json_encode($result));
+        }
     }
 }
 
@@ -38,15 +40,17 @@ function hdsentinel_update() {
 
     message::add('hdsentinel', 'Cette mise à jour peut durer un certain temps : elle va stopper puis supprimer les cron actuels sur les appareils distants, puis renvoyer les scripts, installer les paquets le cron et le lancer.');
     foreach (eqLogic::byType('hdsentinel') as $eqLogic) {
-        $result = array();
-        //$result[] = $eqLogic->stopCron();
-        log::add('hdsentinel', 'info', __('=> Début mise à jour des scripts ', __FILE__) . $eqLogic->getName());
-        $result['stop_remove'] = $eqLogic->removeCron();
-        $result['send'] = $eqLogic->sendFile();
-        $result['install'] = $eqLogic->installDependancy();
-        $result['launch'] = $eqLogic->launchCron();
-        $result['status'] = $eqLogic->statusCron();
-        log::add('hdsentinel', 'info', __('=> Fin mise à jour des scripts ', __FILE__) . $eqLogic->getName() . ' - resultat : ' . json_encode($result));
+        if (!$eqLogic->getConfiguration('manually', false)) {
+            $result = array();
+            //$result[] = $eqLogic->stopCron();
+            log::add('hdsentinel', 'info', __('=> Début mise à jour des scripts ', __FILE__) . $eqLogic->getName());
+            $result['stop_remove'] = $eqLogic->removeCron();
+            $result['send'] = $eqLogic->sendFile();
+            $result['install'] = $eqLogic->installDependancy();
+            $result['launch'] = $eqLogic->launchCron();
+            $result['status'] = $eqLogic->statusCron();
+            log::add('hdsentinel', 'info', __('=> Fin mise à jour des scripts ', __FILE__) . $eqLogic->getName() . ' - resultat : ' . json_encode($result));
+        }
     }
 }
 
